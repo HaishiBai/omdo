1984e91f4f1ec6edd9fe942272f4b5f60558fb7d
Ubuntu <haishi@omdo.omdo.d7.internal.cloudapp.net>
2015-02-22 23:46:34 +0000

    updated SURL format

--- nodejs/wkf-dict/src/wkf-dict.js
+++ nodejs/wkf-dict/src/wkf-dict.js
@@ -1,13 +1,14 @@
 var express = require('express');
 var app = express();
+var oWKFName = 'o.wkf.name';
 
-app.get('/o.wkf/read/*', function (req, res){
-	var path = req.params[0]?req.params[0]:"notfound";
+app.get('/o.wkf/read', function (req, res){
+	var path = req.query[oWKFName]?req.query[oWKFName]:"notfound";
 	var lastDot = path.lastIndexOf('.');
 	if (lastDot > 0)
 	{
 		path = './o.wkf/' + path.substring(0,lastDot).replace(/\./g,'/');
-		res.sendFile(req.params[0] +'.json', {root: path});
+		res.sendFile(req.query[oWKFName] +'.json', {root: path});
 	}
 	else
 		res.status(404).send('Not found');
